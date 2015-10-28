5bf48d6938ce7c7cc693d2666cf5735d6a6b4299
Matt <matt.cavdar@gmail.com>
2015-10-28 16:07:35 -0400

    .include method used to determine if need bananas

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,9 @@ groceries(grocery_list)
 grocery_list.push("rice")
 groceries(grocery_list)
 puts grocery_list.length
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas"
+else
+	puts "You don't need bananas today"
+end
