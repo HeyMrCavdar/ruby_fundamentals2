6c283f5f1d8548a4d8d837c95b0cff2f94cb079e
Matt <matt.cavdar@gmail.com>
2015-10-28 16:22:12 -0400

    .sort method used on array list and puts

--- exercise6.rb
+++ exercise6.rb
@@ -1,20 +1,23 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
 def groceries(list)
 	list.each do |item|
 		puts "* #{item}"
 	end
 end
 
-groceries(grocery_list)
 grocery_list.push("rice")
-groceries(grocery_list)
-puts grocery_list.length
+puts groceries(grocery_list).sort
 
-if grocery_list.include?("bananas")
-	puts "You need to pick up bananas"
-else
-	puts "You don't need bananas today"
-end
+# groceries(grocery_list)
+# puts grocery_list.length
 
-puts grocery_list[1]
+# if grocery_list.include?("bananas")
+# 	puts "You need to pick up bananas"
+# else
+# 	puts "You don't need bananas today"
+# end
+
+# puts grocery_list[1]
+
 
