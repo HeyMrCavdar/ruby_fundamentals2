c5ec6160c6044b0f29e286b1170c0ac3372875d0
Matt <matt.cavdar@gmail.com>
2015-10-28 17:12:45 -0400

    create list of cohorts and students with hash

--- exercise7.rb
+++ exercise7.rb
@@ -1,1 +1,8 @@
-students = (cohort1: 34, cohort2: 42, cohort3: 22)
+students = {cohort1: 34, cohort2: 42, cohort3: 22}
+
+def print_list(class_size)
+	class_size. each do |year, size|
+		puts "#{year}: #{size} students"
+	end
+end
+print_list(students)
