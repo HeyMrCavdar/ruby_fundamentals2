130c23d384298efbc43fc687ad60b0adff6fbb84
Matt <matt.cavdar@gmail.com>
2015-10-28 19:20:49 -0400

    Complete document with bonus incremented variable and each statement

--- exercise7.rb
+++ exercise7.rb
@@ -1,7 +1,18 @@
 $students = {cohort1: 34, cohort2: 42, cohort3: 22}
 
+def print_list(input)
+	input.each do |key, value|
+		puts "#{key}: #{value} students"
+	end
+end
+print_list($students)
+
 $students[:cohort4] = 43
-$students.delete(:cohort2)
+
+puts $students.keys
+
++$students[:cohort4] = 43
+
 def add_5(input)
 	input.each do |key, value|
 		input[key] = value * 1.05
@@ -10,9 +21,20 @@ end
 
 add_5($students)
 $students.each do |key, value|
-	puts "#{key} has #{value} students"
+	puts "#{key} has grown to #{value.round} students"
 end
-# add_5(students)
 
-# puts $students
+$students.delete(:cohort2)
+
+def total_number(input)
+counter = 0
+	input.each do |key, value|
+		counter += value
+	end
+	puts "The total number of students is #{counter.round}"
+end
+
+total_number($students)
+
+
 
